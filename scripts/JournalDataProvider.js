7c9e220428b378bf7db5d914473b17ca6f84ff3b
WilliamMetcalf-37 <william.g.metcalf@gmail.com>
2019-11-19 11:47:24 -0600

    First version of Daily journal

---
+++ scripts/JournalDataProvider.js
@@ -0,0 +1,32 @@
+/*
+ *   Journal data provider for Daily Journal application
+ *
+ *      Holds the raw data about each entry and exports
+ *      functions that other modules can use to filter
+ *      the entries for different purposes.
+ */
+
+// This is the original data. Can't Touch This.
+const journal = [
+  {
+      date: "07/24/2025",
+      concept: "HTML & CSS",
+      entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
+      mood: "Ok"
+  }
+]
+function doIWork(){
+  window.confirm("Do you want to log this into your journal?")
+  }
+/*
+  You export a function that provides a version of the
+  raw data in the format that you want
+*/
+ const useJournalEntries = () => {
+  const sortedByDate = journal.sort(
+      (currentEntry, nextEntry) =>
+          Date.parse(currentEntry.date) - Date.parse(nextEntry.date)
+          
+  )
+  return sortedByDate
+}
